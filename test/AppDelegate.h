7017d259dd1a2e75300ea9c5509f27b3a79f9da7
ziyouzhe4 <592357262@qq.com>
2021-02-04 22:15:48 +0800

    add file

---
+++ test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by majianjie on 2021/2/4.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
